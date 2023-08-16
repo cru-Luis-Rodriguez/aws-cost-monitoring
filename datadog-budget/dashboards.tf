ff6bbf89649976ecc34e6612ceba94cabd3de04b
Luis Rodriguez <luis.rodriguez@cru.org>
2023-08-16 10:03:10 -0400

    update provider to latest and remove deprecated attributes

--- datadog-budget/dashboards.tf
+++ datadog-budget/dashboards.tf
@@ -3,7 +3,6 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
   title        = local.datadog_dashboard_title
   description  = "AWS Cost dashboard with anomalies"
   layout_type  = "ordered"
-  is_read_only = true
 
   template_variable {
     name    = "account_id"
@@ -12,19 +11,17 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
   }
 
   widget {
-    layout = {}
 
     group_definition {
       layout_type = "ordered"
       title       = "Budget"
 
       widget {
-        layout = {}
 
         query_value_definition {
           autoscale = false
           precision = 2
-          time      = {}
+
           title     = "Actual spendings"
 
           request {
@@ -53,12 +50,12 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
         }
       }
       widget {
-        layout = {}
+
 
         query_value_definition {
           autoscale = true
           precision = 2
-          time      = {}
+
           title     = "Forecasted estimates"
 
           request {
@@ -87,10 +84,10 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
         }
       }
       widget {
-        layout = {}
+
 
         toplist_definition {
-          time  = {}
+
           title = "Max of aws.billing.estimated_charges over $account_id by servicename"
 
           request {
@@ -101,17 +98,17 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
     }
   }
   widget {
-    layout = {}
+
 
     group_definition {
       layout_type = "ordered"
       title       = "Anomalies"
 
       widget {
-        layout = {}
+
 
         change_definition {
-          time  = {}
+
           title = "Max of aws.billing.estimated_charges over * by servicename"
 
           request {
@@ -126,11 +123,11 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
         }
       }
       widget {
-        layout = {}
+
 
         alert_graph_definition {
           alert_id = "17757110"
-          time     = {}
+
           title    = "Alert: Abnormal spendings on AWS service {{servicename.name}} on account {{account_id.name}}"
           viz_type = "timeseries"
         }
@@ -138,7 +135,7 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
     }
   }
   widget {
-    layout = {}
+
 
     group_definition {
       layout_type = "ordered"
@@ -147,11 +144,11 @@ resource "datadog_dashboard" "aws_cost_dashboard" {
       dynamic "widget" {
         for_each = var.aws_services
         content {
-          layout = {}
+
 
           timeseries_definition {
             show_legend = false
-            time        = {}
+
             title       = widget.value
 
             request {
